859742b0d0c2bfa45e82e140bda9245607965fc7
Davide Preti <davide.preti86@gmail.com>
2020-08-04 17:03:03 +0200

    Finezze grafiche

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // definisco le variabili con le quali poi andrò a manipolare il codice HTML
 const km = document.getElementById('km');
 const age = document.getElementById('age');
+const discount = document.getElementById('discount');
 const price = document.getElementById('price');
 
 // ottengo il dato "chilometri" tramite comando prompt
@@ -33,11 +34,14 @@ const calcPrice = (kmInput, ageInput) => {
     errorMessage(kmInput, ageInput);
     alert('Inserire solo dati numerici');
   } else if (ageInput < 18) {
-    price.innerHTML = `${(currentPrice - (currentPrice * 20) / 100).toFixed(1)} €`;
+    discount.innerHTML = '20%'
+    price.innerHTML = `${(currentPrice - (currentPrice * 20) / 100).toFixed(2)} €`;
   } else if (ageInput > 65) {
-    price.innerHTML = `${(currentPrice - (currentPrice * 40) / 100).toFixed(1)} €`;
+    discount.innerHTML = '40%';
+    price.innerHTML = `${(currentPrice - (currentPrice * 40) / 100).toFixed(2)} €`;
   } else {
-    price.innerHTML = `${currentPrice.toFixed(1)} €`;
+    discount.innerHTML = 'Nessuno';
+    price.innerHTML = `${currentPrice.toFixed(2)} €`;
   }
 };
 
