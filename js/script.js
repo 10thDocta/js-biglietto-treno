936b5b924bf529b83b6e74ba3b9fac0d2e19421b
Davide Preti <davide.preti86@gmail.com>
2020-08-04 16:55:19 +0200

    Aggiunto arrotond. prima cifra decimale + aggiunta messaggi err

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
-// ottengo il dato "chilometri" tramite comando prompt
-const km = parseInt(prompt('Chilometri da percorrere?'));
-document.getElementById('km').innerHTML = km;
+// definisco le variabili con le quali poi andrò a manipolare il codice HTML
+const km = document.getElementById('km');
+const age = document.getElementById('age');
+const price = document.getElementById('price');
 
+// ottengo il dato "chilometri" tramite comando prompt
+const kmInput = parseInt(prompt('Chilometri da percorrere?'));
+km.innerHTML = kmInput;
 // ottengo il dato "age" tramite comando prompt
-const age = parseInt(prompt('Qual è la tua età?'));
-document.getElementById('age').innerHTML = age;
+const ageInput = parseInt(prompt('Qual è la tua età?'));
+age.innerHTML = ageInput;
 
 const kmPrice = 0.21;
-const currentPrice = km * kmPrice;
+const currentPrice = kmInput * kmPrice;
 
-const calcPrice = (km, age) => {
-  if (isNaN(km) || isNaN(age)) {
-    alert('Inserire solo dati numerici');
-  } else if (age < 18) {
-    return currentPrice - (currentPrice * 20) / 100;
-  } else if (age > 65) {
-    return currentPrice - (currentPrice * 40) / 100;
+// funzione per messaggio di errore in caso di dati non nel formato corretto
+const errorMessage = (kmInput, ageInput) => {
+  if (isNaN(kmInput) && isNaN(ageInput)) {
+    km.innerHTML = '>> Correggere questo dato <<';
+    age.innerHTML = '>> Correggere questo dato <<';
+  } else if (isNaN(kmInput)) {
+    km.innerHTML = '>> Correggere questo dato <<';
   } else {
-    return currentPrice;
+    age.innerHTML = '>> Correggere questo dato <<';
+  }
+
+  price.innerHTML = 'Impossibile da calcolare';
+};
+
+// funzione per il calcolo del prezzo del biglietto
+const calcPrice = (kmInput, ageInput) => {
+  if (isNaN(kmInput) || isNaN(ageInput)) {
+    errorMessage(kmInput, ageInput);
+    alert('Inserire solo dati numerici');
+  } else if (ageInput < 18) {
+    price.innerHTML = `${(currentPrice - (currentPrice * 20) / 100).toFixed(1)} €`;
+  } else if (ageInput > 65) {
+    price.innerHTML = `${(currentPrice - (currentPrice * 40) / 100).toFixed(1)} €`;
+  } else {
+    price.innerHTML = `${currentPrice.toFixed(1)} €`;
   }
 };
 
-// modifico l'elemento h2 con id=prezzo
-document.getElementById('price').innerHTML = `${calcPrice(km, age)}€`;
+calcPrice(kmInput, ageInput);
